2d1b308d8cb298dd4c6efc58453a7779d6b0f778
linux_china <libing.chen@gmail.com>
2020-02-27 16:23:24 -0800

    import greeter.ts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import {sum} from "./math"
+// greeter function from greeter.ts
+import {greeter } from "./greeter";
 import "./demo.css"
 
 console.log(sum(1, 2));
+console.log(greeter("TypeScript"));
