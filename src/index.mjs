b93e198becda2b23219f9b30340d653d7b873e63
linux_china <libing.chen@gmail.com>
2020-04-03 08:58:03 -0700

    index.mjs

---
+++ src/index.mjs
@@ -0,0 +1,12 @@
+//esm module
+import {sum} from "./math.mjs"
+// typescript module
+import {greeter} from "./greeter";
+// cjs module
+import * as cjs1 from "./cjs1.js"
+
+console.log(sum(1, 2));
+
+console.log(greeter("TypeScript"));
+
+cjs1.commonjs_hello();
