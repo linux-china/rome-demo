9e88fc08daa8afbb52d0ff7c39b18309285f6b61
linux_china <libing.chen@gmail.com>
2020-04-19 16:38:33 -0700

    lint fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const cjs1 = require("./cjs1");
+const cjs1 = require('./cjs1');
 
 (async () => {
-    let math = await import("./math.mjs")
-    console.log(math.sum(1, 2))
-    cjs1.hello()
+  let math = await import('./math.mjs');
+  console.log(math.sum(1, 2));
+  cjs1.hello();
 })();
